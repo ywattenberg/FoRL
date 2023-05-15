84088eb106400b893806e463b60c52f886996f56
Thomas Rupf <thomas.rupf@inf.ethz.ch>
2023-05-15 20:39:02 +0200

    x

--- train_eval_one.py
+++ train_eval_one.py
@@ -17,7 +17,7 @@ def prog_print(msg):
     print("=================================")
 
 
-def get_train_cmd(Model, env, Model_path):
+def get_train_cmd(python_path, Model, env, Model_path):
     return [
         python_path,
         "rl_zoo3/train.py",
@@ -37,7 +37,7 @@ def get_train_cmd(Model, env, Model_path):
     ]
 
 
-def get_eval_cmd(Model, train_env, test_env, Model_path, eval_folder):
+def get_eval_cmd(python_path, Model, train_env, test_env, Model_path, eval_folder):
     return [
         python_path,
         "rl_zoo3/enjoy.py",
@@ -73,34 +73,34 @@ def main():
 
     # python .\train.py --algo a2c --env FoRLMountainCarRandomNormal-v0 --device cuda --vec-env subproc --progress -conf ..\FoRL_conf\a2c.yml
     prog_print(f"Training deterministic env for {Model} in {env}")
-    res = subprocess.Popen(get_train_cmd(Model, env, Model_path))
+    res = subprocess.Popen(get_train_cmd(python_path, Model, env, Model_path))
     res.wait()
 
     prog_print(f"Training random env for {Model} in {env}")
-    res = subprocess.Popen(get_train_cmd(Model, get_random_name(env), Model_path))
+    res = subprocess.Popen(get_train_cmd(python_path, Model, get_random_name(env), Model_path))
     res.wait()
 
     prog_print(f"Eval DD for {Model} in {env}")
-    res = subprocess.Popen(get_eval_cmd(Model, env, env, Model_path, eval_folder))
+    res = subprocess.Popen(get_eval_cmd(python_path, Model, env, env, Model_path, eval_folder))
     res.wait()
 
     prog_print(f"Eval DR for {Model} in {env}")
-    res = subprocess.Popen(get_eval_cmd(Model, env, get_random_name(env), Model_path, eval_folder))
+    res = subprocess.Popen(get_eval_cmd(python_path, Model, env, get_random_name(env), Model_path, eval_folder))
     res.wait()
 
     prog_print(f"Eval RR for {Model} in {env}")
     res = subprocess.Popen(
-        get_eval_cmd(Model, get_random_name(env), get_random_name(env), Model_path, eval_folder)
+        get_eval_cmd(python_path, Model, get_random_name(env), get_random_name(env), Model_path, eval_folder)
     )
     res.wait()
 
     prog_print(f"Eval DE for {Model} in {env}")
-    res = subprocess.Popen(get_eval_cmd(Model, env, get_extreme_name(env), Model_path, eval_folder))
+    res = subprocess.Popen(get_eval_cmd(python_path, Model, env, get_extreme_name(env), Model_path, eval_folder))
     res.wait()
 
     prog_print(f"Eval RE for {Model} in {env}")
     res = subprocess.Popen(
-        get_eval_cmd(Model, get_random_name(env), get_extreme_name(env), Model_path, eval_folder)
+        get_eval_cmd(python_path, Model, get_random_name(env), get_extreme_name(env), Model_path, eval_folder)
     )
     res.wait()
 
